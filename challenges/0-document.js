dda9b9d71e343f378a8ca37a2694789c632f73ad
Carrie <carriearoberts95@gmail.com>
2021-09-14 13:13:15 +0100

    Committing my solution

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,9 @@
 // TODO (Ex.0): document and write tests for this function
-
-function asNegative(n) {
+/**
+ * Given a number, returns a negative version of the number.
+ * @param {number} n - input number
+ * @returns {number} `n` in negative form
+ */
+ function asNegative(n) {
   // ...
 }
-
-
